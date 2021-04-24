7063fab8ea75bbe3b5b2c87b89ded391bd7aa4df
Best\Sorathorn <sorathorn.kh@mail.kmutt.ac.th>
2021-04-24 16:23:17 +0700

    no message

---
+++ 5.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day, time;
+            string Breakfast Set, Weekend Set, Coffee;
+            day = int.Parse(Console.ReadLine());
+            time = int.Parse(Console.ReadLine());
+            switch ("Breakfast Set")
+            {
+                case 1:
+                    if (time > 11)
+                    {
+                        Console.WriteLine("Sorry your order is not available")
+                    }
+                    break;
+                case 2:
+                    if (Breakfast Set = 0)
+                    {
+                        Console.WriteLine("Sorry your order is out of stock")
+                    }
+                    break;
+
+            }
+            switch ("Weekend Set")
+            {
+                case 1:
+                    if (day >= 6)
+                    {
+                        Console.WriteLine("Sorry your order is not available")
+                    }
+                    break;
+                case 2:
+                    if (Weekend Set = 0)
+                    {
+                        Console.WriteLine("Sorry your order is out of stock")
+                    }
+                    break;
+            }
+            switch ("Coffee")
+            {
+                case 1:
+                    if (Coffee = 0)
+                    {
+                        Console.WriteLine("Sorry your order is out of stock")
+                    }
+                    break;
+            }
+        }
+    }
+}
